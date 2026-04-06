93a94282100766f579d001fb97d9fccb81a14b02
Manav Anreja <anrejamanav@gmail.com>
2026-04-06 15:10:13 +0530

    Intialization frontend with tailwind config

---
+++ food-delivery-frontend/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-blue-500 text-2xl'>Food-delivey-frontend</h1>
+    </>
+  )
+}
+
+export default App
