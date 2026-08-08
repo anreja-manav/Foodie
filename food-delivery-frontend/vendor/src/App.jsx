3dbc8915b53612e473600478aa8f8da77118b583
Manav Anreja <anrejamanav@gmail.com>
2026-08-08 22:40:00 +0530

    Vendor Project Started

---
+++ food-delivery-frontend/vendor/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black h-350'>
+      <h1 className='text-orange-400'>Vendor Page</h1>
+    </div>
+  )
+}
+
+export default App
