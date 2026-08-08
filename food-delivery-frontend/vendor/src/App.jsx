470919d1db3156ad247099db21d2b0b4326338ff
Manav Anreja <anrejamanav@gmail.com>
2026-08-08 23:39:11 +0530

    Login or register page created for vendor

--- food-delivery-frontend/vendor/src/App.jsx
+++ food-delivery-frontend/vendor/src/App.jsx
@@ -1,11 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { toast, ToastContainer } from "react-toastify";
 import './App.css'
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ForgotPassword from './pages/Forgot_Password';
+import Verify from './pages/Verify';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+export const MyContext = React.createContext();
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, [isLogin]);
+
+  const alertBox = (type, msg) => {
+      if (type === "success") toast.success(msg);
+      if (type === "error") toast.error(msg);
+    };
+
+  const values = {
+    alertBox,
+    isLogin,
+    setIsLogin,
+    windowWidth,
+  }
 
   return (
-    <div className='bg-black h-350'>
-      <h1 className='text-orange-400'>Vendor Page</h1>
-    </div>
+    <> 
+      <BrowserRouter>
+        <MyContext.Provider value={values}>
+          <main >
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path='/verify' element={<Verify />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/forgot_password/confirm' element={<ForgotPassword />} />
+            </Routes>
+          </main>
+        </MyContext.Provider>
+        <ToastContainer />
+      </BrowserRouter>
+    </>
   )
 }
 
